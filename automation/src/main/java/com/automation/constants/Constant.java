72f0aa129dc24d7e6cdd9a13d5c820617df49cab
tiddhacker <sounakghosh62@gmail.com>
2022-08-09 00:09:23 +0530

    updates

--- automation/src/main/java/com/automation/constants/Constant.java
+++ automation/src/main/java/com/automation/constants/Constant.java
@@ -6,6 +6,7 @@ public class Constant {
 	public final static String CONFIG_FILE_PROPERTIES_DIRECTORY=System.getProperty("user.dir")+"/src/main/java/com/automation/config/";
 	public final static String IMAGE_DIRECTORY= System.getProperty("user.dir")+"/target/report/imgs/";
 	public final static String DATA_FACTORY= System.getProperty("user.dir")+"/src/main/java/com/automation/dataFactory/";
+	public final static String JSON_FILE_DIR= System.getProperty("user.dir")+"/src/main/java/com/automation/api/testJson/";
 
 	//Waits
 	public final static long EXPLICIT_WAIT_DURATION=10;
---
+++ automation/src/main/java/com/automation/utils/JsonUtils.java
@@ -0,0 +1,62 @@
+package com.automation.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+
+import com.automation.constants.Constant;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class JsonUtils {
+	
+	public static void main(String[]args) throws ParseException, JSONException, IOException {
+		
+		JsonObject obj = getJsonObject("testJson");
+		System.out.println(obj.toString());
+		
+	}
+
+	public static JsonObject readJsonFile(String fileName) throws FileNotFoundException {
+		File jsonInputFile;
+		jsonInputFile = new File(Constant.JSON_FILE_DIR + fileName + ".json");
+		InputStream is = new FileInputStream(jsonInputFile);
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+		JsonReader reader = Json.createReader(bReader);
+		JsonObject jsonFileObject = reader.readObject();
+		reader.close();
+		return jsonFileObject;
+	}
+
+	public static synchronized JsonObject getJsonObject(String fileName) {
+
+		JsonObject object = null;
+		try {
+			object = readJsonFile(fileName);
+
+		} catch (Exception e) {
+			Assert.fail("Failed to read JSON filename: " + fileName);
+		}
+
+		return object;
+	}
+	
+	
+}
